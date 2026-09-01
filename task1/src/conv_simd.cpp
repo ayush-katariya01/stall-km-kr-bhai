763569e24dd8a090f13dbc179a87f64c0192a093
sunil <sunilattri345@gmail.com>
2026-09-01 15:40:14 +0530

    task1 simd finalised here

--- task1/src/conv_simd.cpp
+++ task1/src/conv_simd.cpp
@@ -21,96 +21,30 @@ void conv_simd(const float* in, float* out, const float* ker,
     //     }
     // }
 
-
-    //start
-
-    int k2=K*K;
     __m256 a,b,c;
     int  m=0;
     const int p = K / 2;
     const int in_stride = W + 2 * p;  // padded row stride
+    int stride1,stride2;
 
     for (int oy = 0; oy < H; ++oy) {
-        if(oy!=H-1){
-            for (int ox = 0; ox < W; ox=ox+8) {
-                c=_mm256_setzero_ps();
-                
-                for(int j=0;j<K;j++){
-                    int stride=(oy + j) * in_stride;
-                    for(int i=0;i<K;i++){
-                        a=_mm256_load_ps(&in[stride + (ox + i)]);;
-                        b=_mm256_set1_ps(ker[i]);
-                        c=_mm256_add_ps(c,_mm256_mul_ps(a,b));
-                    }
-                }
-                
-                _mm256_storeu_ps(out+m, c);
-                if((W-ox)!=8){
-                    m=m+8;
-                    // out[oy * W + ox] = acc;
-                }
-                else{
-                    m=m+K-1;
+        for (int ox = 0; ox < W; ox=ox+8) {
+            c=_mm256_setzero_ps();
+            
+            for(int j=0;j<K;j++){
+            stride1=(oy + j) * in_stride;
+            stride2=j * K;
+            for(int i=0;i<K;i++){
+                    a=_mm256_load_ps(&in[stride1 + (ox + i)]);;
+                    b=_mm256_set1_ps(ker[stride2 + i]);
+                    c=_mm256_add_ps(c,_mm256_mul_ps(a,b));
                 }
             }
-        }
-        else{
-            for (int ox = 0; ox < W; ox=ox+8) {
-                c=_mm256_setzero_ps();
-                
-                for(int j=0;j<K;j++){
-                    int stride=(oy + j) * in_stride;
-                    if(j!=K-1){
-                        for(int i=0;i<K;i++){
-                            a=_mm256_load_ps(&in[stride + (ox + i)]);
-                            b=_mm256_set1_ps(ker[i]);
-                            c=_mm256_add_ps(c,_mm256_mul_ps(a,b));
-                        }
-                    }
-                    else{
-                        if((W-ox)>8){
-                            for(int i=0;i<K;i++){
-                                a=_mm256_load_ps(&in[stride + (ox + i)]);
-                                b=_mm256_set1_ps(ker[i]);
-                                c=_mm256_add_ps(c,_mm256_mul_ps(a,b));
-                            }
-                        }
-                        else{
-                            float jugad[(W-ox)+K-1];
-
-                            for(int jugadIndex=0;jugadIndex<8;jugadIndex++){
-                                jugad[jugadIndex]=in[stride + (ox + jugadIndex)];
-                            }
-                            for(int jugadIndex=8;jugadIndex<8+K-1;jugadIndex++){
-                                jugad[jugadIndex]=in[stride + (ox + jugadIndex)];
-                            }
-                            for(int i=0;i<K;i++){
-                                a=_mm256_load_ps(jugad+i);
-                                b=_mm256_set1_ps(ker[i]);
-                                c=_mm256_add_ps(c,_mm256_mul_ps(a,b));
-                            }
-                            _mm256_storeu_ps(jugad, c);
-
-                            for(int i=0;m<H*W;m++)
-                            out[m]=jugad[i++];
-                        }
-                    }
-                }
-                
-                _mm256_storeu_ps(out+m, c);
-                if((W-ox)!=8){
-                    m=m+8;
-                    // out[oy * W + ox] = acc;
-                }
-                else{
-                    m=m+K-1;
-                }
-            }
+            _mm256_store_ps(out+m, c);
+            m=m+8;
         }
     }    
-    
-    //end
-
+                
     // TODO(student): replace this placeholder with your AVX2 implementation.
     // conv_naive(in, out, ker, H, W, K);
 }

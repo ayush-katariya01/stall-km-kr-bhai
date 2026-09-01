2afef457722a83457113efe1b08eff27f614d73c
sunil <sunilattri345@gmail.com>
2026-09-01 15:16:11 +0530

    lets revert

--- task1/src/conv_simd.cpp
+++ task1/src/conv_simd.cpp
@@ -21,6 +21,9 @@ void conv_simd(const float* in, float* out, const float* ker,
     //     }
     // }
 
+
+    //start
+
     int k2=K*K;
     __m256 a,b,c;
     int  m=0;
@@ -28,24 +31,86 @@ void conv_simd(const float* in, float* out, const float* ker,
     const int in_stride = W + 2 * p;  // padded row stride
 
     for (int oy = 0; oy < H; ++oy) {
-        
-        for (int ox = 0; ox < W; ox=ox+8) {
-            float acc = 0.0f;
-
-            for(int j=0;j<K;j++){
-            for(int i=0;i<K;i++){
-                    a=_mm256_load_ps(&in[(oy + j) * in_stride + (ox + i)]);;
-                    b=_mm256_set1_ps(ker[i]);
-                    c=_mm256_add_ps(c,_mm256_mul_ps(a,b));
+        if(oy!=H-1){
+            for (int ox = 0; ox < W; ox=ox+8) {
+                c=_mm256_setzero_ps();
+                
+                for(int j=0;j<K;j++){
+                    int stride=(oy + j) * in_stride;
+                    for(int i=0;i<K;i++){
+                        a=_mm256_load_ps(&in[stride + (ox + i)]);;
+                        b=_mm256_set1_ps(ker[i]);
+                        c=_mm256_add_ps(c,_mm256_mul_ps(a,b));
+                    }
+                }
+                
+                _mm256_storeu_ps(out+m, c);
+                if((W-ox)!=8){
+                    m=m+8;
+                    // out[oy * W + ox] = acc;
+                }
+                else{
+                    m=m+K-1;
                 }
             }
+        }
+        else{
+            for (int ox = 0; ox < W; ox=ox+8) {
+                c=_mm256_setzero_ps();
+                
+                for(int j=0;j<K;j++){
+                    int stride=(oy + j) * in_stride;
+                    if(j!=K-1){
+                        for(int i=0;i<K;i++){
+                            a=_mm256_load_ps(&in[stride + (ox + i)]);
+                            b=_mm256_set1_ps(ker[i]);
+                            c=_mm256_add_ps(c,_mm256_mul_ps(a,b));
+                        }
+                    }
+                    else{
+                        if((W-ox)>8){
+                            for(int i=0;i<K;i++){
+                                a=_mm256_load_ps(&in[stride + (ox + i)]);
+                                b=_mm256_set1_ps(ker[i]);
+                                c=_mm256_add_ps(c,_mm256_mul_ps(a,b));
+                            }
+                        }
+                        else{
+                            float jugad[(W-ox)+K-1];
 
-            _mm256_storeu_ps(out+m, c);
-            m=m+8;
-            // out[oy * W + ox] = acc;
+                            for(int jugadIndex=0;jugadIndex<8;jugadIndex++){
+                                jugad[jugadIndex]=in[stride + (ox + jugadIndex)];
+                            }
+                            for(int jugadIndex=8;jugadIndex<8+K-1;jugadIndex++){
+                                jugad[jugadIndex]=in[stride + (ox + jugadIndex)];
+                            }
+                            for(int i=0;i<K;i++){
+                                a=_mm256_load_ps(jugad+i);
+                                b=_mm256_set1_ps(ker[i]);
+                                c=_mm256_add_ps(c,_mm256_mul_ps(a,b));
+                            }
+                            _mm256_storeu_ps(jugad, c);
+
+                            for(int i=0;m<H*W;m++)
+                            out[m]=jugad[i++];
+                        }
+                    }
+                }
+                
+                _mm256_storeu_ps(out+m, c);
+                if((W-ox)!=8){
+                    m=m+8;
+                    // out[oy * W + ox] = acc;
+                }
+                else{
+                    m=m+K-1;
+                }
+            }
         }
     }    
-                
+    
+    //end
+
     // TODO(student): replace this placeholder with your AVX2 implementation.
     // conv_naive(in, out, ker, H, W, K);
 }
